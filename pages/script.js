5baa3fe7eff24558f048e1acfa8e5d6b5f950b6d
Saidraximbek Murodilov <113995528+Saidraximbek@users.noreply.github.com>
2024-12-10 09:18:43 +0500

    myteam website full version imported

---
+++ pages/script.js
@@ -0,0 +1,9 @@
+
+// const nameResult = document.getElementById("").value;
+
+function myFunction() {
+    var x = document.getElementById("name").value;
+    document.getElementById("result-span").innerHTML = x;
+  }
+
+console.log(nameResult)
